469a6ae8691f9e82c116b11f812b8ea3d4b71265
yueyanbin <yyb_home@126.com>
2019-12-02 15:58:12 +0800

    parents

--- page.config.js
+++ page.config.js
@@ -19,5 +19,20 @@ module.exports = [
     name: 'polyfill',
     html: 'polyfill/polyfill.html',
     jsEntry: 'polyfill/polyfill.js'
+  },
+  {
+    name: 'difference',
+    html: 'difference/difference.html',
+    jsEntry: 'difference/difference.js'
+  },
+  {
+    name: 'freeassessment',
+    html: 'freeassessment/freeassessment.html',
+    jsEntry: 'freeassessment/freeassessment.js'
+  },
+  {
+    name: 'parents',
+    html: 'parents/parents.html',
+    jsEntry: 'parents/parents.js'
   }
 ]
